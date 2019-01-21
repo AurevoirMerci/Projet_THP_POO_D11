445175a369d70c296c752c6d8f19d2f7d9bc4a59
geoffrey03 <geoffrey.poirrier@essec.edu>
2019-01-21 13:31:09 +0100

    version geo

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,31 @@
+require 'pry'
+require 'time'
+
 class Event
+  attr_accessor :start_date, :lenght, :title, :attendees
+  @@event_infos = []
+
+  def initialize(date, durée, titre, invités)
+    @start_date = date
+    @lenght = durée
+    @title = titre
+    @attendees = invités
+    date = Time.parse("#{date}")
+    @@event_infos << date << durée << titre << invités
+    postpone_24h(date)
+    return date
+  end
+
+  def postpone_24h(date)
+  	date2 = date + 60 * 60 * 24
+  	@@event_infos.delete(date)
+  	@@event_infos << date2
+  end
+
+  def self.all
+  	return @@event_infos
+  end
 
 end
+
+binding.pry
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 class User
+	attr_accessor :email
+	@@user_count = []
+
+	def initialize(email_to_save)
+      @email = email_to_save
+      @@user_count << email_to_save
+	end
+
+    def self.all
+      return @@user_count
+    end
 
 end
+
+binding.pry
